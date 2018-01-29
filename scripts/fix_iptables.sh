b99d798d529a0893446882c6b7d3db73555a1371
Adrian Hyncica <adrianhyncica@gmail.com>
2018-01-29 17:27:50 +0000

    change fix iptables script
    get the random subnet from file
    use the subnet to set iptables routing

--- scripts/fix_iptables.sh
+++ scripts/fix_iptables.sh
@@ -3,7 +3,8 @@
 # called by pivpnDebug.sh
 
 IPv4dev=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++)if($i~/dev/)print $(i+1)}')
+IPv4SubNet=$(</etc/pivpn/vpnSubNet.txt)
 iptables -t nat -F
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o ${IPv4dev} -j MASQUERADE
+iptables -t nat -A POSTROUTING -s "$IPv4SubNet.0/24" -o ${IPv4dev} -j MASQUERADE
 iptables-save > /etc/iptables/rules.v4
 iptables-restore < /etc/iptables/rules.v4
